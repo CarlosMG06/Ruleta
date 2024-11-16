dcaaf312546a9436785a5a4aea529f7357a0e4e8
CarlosMG06 <carlos.abmg06@gmail.com>
2024-11-16 23:06:33 +0100

    número actual y ajustar ruleta después de girar

--- main.py
+++ main.py
@@ -15,16 +15,20 @@ RED = (255, 0, 0)
 DARK_GREEN = (0, 85, 17)
 GREEN = (0, 255, 0)
 GOLD = (255, 128, 0)
+DARK_GOLD = (128, 64, 0)
 #BLUE  = (0, 0, 255)
 #PURPLE = (128, 0, 128)
 #ORANGE = (255, 165, 0) 
 BROWN = (102, 68, 34)
 
-roulette = {"center": (180,180), "angle_offset": 0, "spin_speed": 0, "spin_acc": -100, "about_to_spin": False}
+roulette = {"center": (180,180), "radius": 150,
+"angle_offset": 360/37/2, "spin_speed": 0, "spin_acc": -100,
+"about_to_spin": False, "spinning": False, "stopped_spinning": False}
 red_numbers = [3, 6, 7, 8, 12, 14, 16, 17, 19, 21, 22, 25, 26, 28, 31, 33, 34, 36]
 mouse = {"x": -1, "y": -1, 
 "pressed": False, "held": False, "released": False}
 spin_button = {"x": 140, "y": 400, "width": 80, "height": 30}
+current_number = 0
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -68,19 +72,26 @@ def app_events():
 def app_run():
     delta_time = clock.get_time() / 1000.0
     
-    if utils.is_point_in_rect(mouse, spin_button) and roulette["spin_speed"] <= 1:
+    if utils.is_point_in_rect(mouse, spin_button) and not roulette["spinning"]:
         if mouse["pressed"]:
             roulette["spin_speed"] = -50
             roulette["about_to_spin"] = True
-        elif mouse["released"] and roulette["about_to_spin"]:
-            roulette["spin_speed"] = random.randrange(40000,50000)/100
+        if mouse["released"] and roulette["about_to_spin"]:
+            acc = abs(roulette["spin_acc"])
+            angular_displacement = random.randint(540,900) #Gira entre 540 i 900 graus
+            roulette["spin_speed"] = math.sqrt(angular_displacement*2/acc)*acc #Càlcul MCUA
+            roulette["spinning"] = True
             roulette["about_to_spin"] = False
     elif roulette["about_to_spin"]:
-        roulette["spin_speed"] = math.sqrt((-50)**2 - roulette["spin_speed"]**2)
+        # Si el mouse es mou fora del botó abans de deixar anar, la ruleta torna a la seva posició inicial
+        roulette["spin_speed"] = math.sqrt((-50)**2 - roulette["spin_speed"]**2) #Càlcul MCUA
         roulette["about_to_spin"] = False
 
     if roulette["spin_speed"] != 0:
         spin_roulette(delta_time)
+    elif roulette["stopped_spinning"]:
+        adjust_roulette()
+        roulette["stopped_spinning"] = False
     
     mouse["pressed"] = False
     mouse["released"] = False
@@ -94,6 +105,11 @@ def app_draw():
 
     draw_roulette()
 
+    text_number = fontBold.render(f"Current number: {str(current_number)}", True, WHITE)
+    text_number_rect = text_number.get_rect()
+    text_number_rect.center = (180,350)
+    screen.blit(text_number, text_number_rect)
+
     # Actualitzar el dibuix a la finestra
     pygame.display.update()
 
@@ -106,7 +122,7 @@ def draw_spin_button():
         fill_color = GOLD
         border_color = BLACK
         text_color = BLACK
-    elif roulette["spin_speed"] > 0:
+    elif roulette["spinning"]:
         fill_color = DARK_GRAY
         border_color = GRAY
         text_color = GRAY
@@ -118,10 +134,12 @@ def draw_spin_button():
 
 def draw_roulette():
     c = roulette["center"]
-    pygame.draw.circle(screen, BLACK, c, 150, 5)
-    pygame.draw.circle(screen, BROWN, c, 100)
+    r = roulette["radius"]
+    pygame.draw.circle(screen, DARK_GOLD, c, r+5, 5)
+    pygame.draw.circle(screen, BROWN, c, r-50)
+    pygame.draw.polygon(screen, GOLD, [(338,180),(353,188),(353,172)])
     for n in range(37):
-        angle = 360/37*n + roulette["angle_offset"]
+        angle = (360/37*n + roulette["angle_offset"]) % 360
         if n == 0:
             color = GREEN
         elif n in red_numbers:
@@ -129,33 +147,53 @@ def draw_roulette():
         else:
             color = BLACK
         
-        p0 = utils.point_on_circle(c, 130, angle)
-        p1 = utils.point_on_circle(c, 150, angle)
+        p0 = utils.point_on_circle(c, r-20, angle)
+        p1 = utils.point_on_circle(c, r, angle)
 
         prev_angle = angle - 360/37
-        prev_0 = utils.point_on_circle(c, 130, prev_angle)
-        prev_1 = utils.point_on_circle(c, 150, prev_angle)
+        prev_0 = utils.point_on_circle(c, r-20, prev_angle)
+        prev_1 = utils.point_on_circle(c, r, prev_angle)
+
+        if roulette["spin_speed"] == 0 and not roulette["about_to_spin"] and prev_angle + angle < 360/37:
+            global current_number
+            current_number = n
 
         points = [p0,p1,prev_1,prev_0]
         pygame.draw.polygon(screen, color, points)
         pygame.draw.polygon(screen, LIGHT_GRAY, points, 3)
 
-        center = (int((p0[0]+prev_1[0])/2), int((p0[1]+prev_1[1])/2))
+        polygon_center = (int((p0[0]+prev_1[0])/2), int((p0[1]+prev_1[1])/2))
 
         text_angle = -angle - 90 + 360/37/2
         text_n = fontBold.render(str(n), True, WHITE)
         text_n_rotated = pygame.transform.rotate(text_n, text_angle)
         text_n_rotated_rect = text_n_rotated.get_rect()
-        text_n_rotated_rect.center = center
+        text_n_rotated_rect.center = polygon_center
         screen.blit(text_n_rotated, text_n_rotated_rect)
 
+# Girar la ruleta
 def spin_roulette(delta_time):
     sign = 1 if roulette["spin_speed"] > 0 else -1
-    if abs(roulette["spin_speed"]) > 1:
+    if abs(roulette["spin_speed"]) > 0.5:
         roulette["spin_speed"] += sign * roulette["spin_acc"] * delta_time
     else:
+        if roulette["spinning"]:
+            roulette["spinning"] = False 
+            roulette["stopped_spinning"] = True
+        roulette["angle_offset"] %= 360
         roulette["spin_speed"] = 0
     roulette["angle_offset"] += roulette["spin_speed"] * delta_time
+
+# Ajustar la ruleta després de girar per alinear el número que ha sortit amb la fletxa
+def adjust_roulette():
+    adjustment = (roulette["angle_offset"] - 360/37/2) % (360/37)
+    if adjustment >= 360/37/2:
+        adjustment -= 360/37
+        sign = 1
+    else:
+        sign = -1
+    if abs(adjustment) > 360/37/10:
+        roulette["spin_speed"] = sign * math.sqrt(200 * abs(adjustment))
 
 if __name__ == "__main__":
     main()
