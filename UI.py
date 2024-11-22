d178eb2068350f9eb013fcd00935cf86a586d8e9
RubenBellidoNavarro <ruben.bellido.navarro@gmail.com>
2024-11-22 17:57:16 +0100

    cambio para arreglar posición de objetos Rect en las casillas

--- UI.py
+++ UI.py
@@ -138,7 +138,7 @@ def init_grid_surface():
         text_n_rotated_rect = text_n_rotated.get_rect(center=(center_x, center_y))
         grid_surface.blit(text_n_rotated, text_n_rotated_rect)
 
-        board_cell_areas[str(n)] = {"rect": {"x": x + board['x'] + board['grid_x'], "y": y + board['y'], "width": c_w, "height": c_h}}
+        board_cell_areas[str(n)] = {"rect": {"x": x + board['x'] + board['grid_x'], "y": y + board['y'] + board['grid_y'], "width": c_w, "height": c_h}}
 
     # Cel·les inferiors
     for bottom_cell in range(4):
@@ -165,7 +165,7 @@ def init_grid_surface():
             draw.polygon(grid_surface, color, diamond_points)
             draw.polygon(grid_surface, LIGHT_GRAY, diamond_points, 3)
 
-        board_cell_areas[string] = {"rect": {"x": x + board['x'] + board['grid_x'], "y": y + board['y'], "width": c_w * 3, "height": c_h}}
+        board_cell_areas[string] = {"rect": {"x": x + board['x'] + board['grid_x'], "y": y + board['y'] + board['grid_y'], "width": c_w * 3, "height": c_h}}
 
     #Quadre extern
     draw.rect(grid_surface, LIGHT_GRAY, (0, 0, grid_size[0], grid_size[1]), 3)
